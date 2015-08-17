0d8e8a489fd1b6e6eb595772771b5176f0f337bf
Irfan <ihamid@salesforce.com>
2015-08-17 08:28:43 -0700

    Lower required version of cmake to 2.8.7. Remove Enable.hpp since those macros aren't required with the new arch/ directory structure for intrinsics internals.

--- src/lib/arch/avx/AvxInternals.cpp
+++ src/lib/arch/avx/AvxInternals.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "Enable.hpp"
 #include <immintrin.h>
 #include <cmath>
 #include "AvxInternals.hpp"
